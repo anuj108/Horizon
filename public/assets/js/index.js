ab62d389bdcc549f8b95010e350aa13c4a4da8b8
AnUj <shakya.anuj910@gmail.com>
2022-02-03 17:46:49 +0530

    merged brnaches

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,16 +1,19 @@
+// kingshuk
 const hamburger = document.querySelector('.hamburger');
-const dropdown = document.querySelector('.options');
-const options = document.querySelectorAll('.options li');
+const innerOptions = document.querySelector('.inner-options');
+const innerOptionsList = document.querySelectorAll('.inner-options li');
+const outerOptions = document.querySelector('.outer-options');
 
 
 window.onload=()=>{
-    const tab_switchers=document.querySelectorAll('[data-switcher');
-    for(let i=0;i<tab_switchers.length;i++)
-    {
-        const tab_switcher=tab_switchers[i];
-        const page_id=tab_switcher.dataset.tab;
+    const tab_switchers=document.querySelectorAll('[data-switcher]');
+    tab_switchers.forEach( tab_switcher => {
+       
+
+        const curr_page_id = tab_switcher.dataset.tab;
 
         tab_switcher.addEventListener('click',()=>{
+            
             document.querySelector('.tab.is-active').classList.remove('is-active');
             tab_switcher.parentNode.classList.add('is-active');
 
@@ -22,9 +25,8 @@ window.onload=()=>{
                     link.style.animation= '';
                 else    
                     link.style.animation = `navLinkFade 0.5s ease forwards ${index/4 + 0.45}s`;
-            });
-        })
-    }
+    });
+
 }
 
 function SwitchPage(page_id){
@@ -35,9 +37,6 @@ function SwitchPage(page_id){
     next_page.classList.add('is-active');
 }
 
-
-// Navbar Animation
-
 hamburger.addEventListener('click',() =>
 {
     hamburger.classList.toggle('active-ham');
@@ -47,9 +46,79 @@ hamburger.addEventListener('click',() =>
         if(link.style.animation)
             link.style.animation= '';
         else    
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index/4 + 0.45}s`;
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index/4  }s`;
     });
 });
+
+// kingshuk
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ANUJ
+// const hamburger = document.querySelector('.hamburger');
+// const dropdown = document.querySelector('.options');
+// const options = document.querySelectorAll('.options li');
+
+
+// window.onload=()=>{
+//     const tab_switchers=document.querySelectorAll('[data-switcher');
+//     for(let i=0;i<tab_switchers.length;i++)
+//     {
+//         const tab_switcher=tab_switchers[i];
+//         const page_id=tab_switcher.dataset.tab;
+
+//         tab_switcher.addEventListener('click',()=>{
+//             document.querySelector('.tab.is-active').classList.remove('is-active');
+//             tab_switcher.parentNode.classList.add('is-active');
+
+//             SwitchPage(page_id);
+//             hamburger.classList.remove('active-ham');
+//             dropdown.classList.remove('outside');
+//             options.forEach((link, index) =>{
+//                 if(link.style.animation)
+//                     link.style.animation= '';
+//                 else    
+//                     link.style.animation = `navLinkFade 0.5s ease forwards ${index/4 + 0.45}s`;
+//             });
+//         })
+//     }
+// }
+
+// function SwitchPage(page_id){
+//     const current_page=document.querySelector('.page.is-active');
+//     current_page.classList.remove('is-active');
+
+//     const next_page=document.querySelector(`.page[data-page="${page_id}"]`);
+//     next_page.classList.add('is-active');
+// }
+
+
+// // Navbar Animation
+
+// hamburger.addEventListener('click',() =>
+// {
+//     hamburger.classList.toggle('active-ham');
+//     dropdown.classList.toggle('outside');
+
+//     options.forEach((link, index) =>{
+//         if(link.style.animation)
+//             link.style.animation= '';
+//         else    
+//             link.style.animation = `navLinkFade 0.5s ease forwards ${index/4 + 0.45}s`;
+//     });
+// });
 // const links = querySelectorAll('.tab');
 // links.forEach(link => {
 //     link.addEventListener('click', () => {
@@ -63,3 +132,4 @@ hamburger.addEventListener('click',() =>
 //         dropdown.classList.remove('outside');
 //     }
 // })
+// ANUJ
