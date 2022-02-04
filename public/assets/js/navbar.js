be8c4a9b3e9db090ce4900df9516f861217a1629
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2022-02-04 16:27:40 +0530

    navbar done

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -14,7 +14,7 @@ hamburger.addEventListener('click',() =>
         if(link.style.animation)
             link.style.animation= '';
         else    
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index/4 + 0.45}s`;
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index/4 }s`;
     });
 });
 
